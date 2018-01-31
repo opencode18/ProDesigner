4373c017ff58cfb0a908ddd64a45cb88ed3558bb
arpit <tipra2614@gmail.com>
2018-02-01 00:41:23 +0530

    Solved some issues

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
-projects = document.querySelectorAll(".projects li");
-pen = document.querySelectorAll(".project-links p");
-links = document.querySelectorAll(".links")
-var select;
+var projects = document.querySelectorAll(".projects li");
+var pen = document.querySelectorAll(".project-links p");
+var links = document.querySelectorAll(".links");
 
 
 
 for(var i = 0; i < projects.length; i++)
 {
-    select = i;
-    projects[i].addEventListener("click", function()
-    {
-        this.classList.add("selected");
-        // window.scrollTo(0, 0);
-        window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-        for(var j = 0; j < projects.length; j++)
-        {
-            if(projects[j] != this)
-            {
-                projects[j].classList.remove("selected");
-                links[j].style.display = "none";
-            }
-            else
-                links[j].style.display = "block";
-        }
-    });
+    projects[i].addEventListener("click", select);
 }
 
-for(var i = 0; i < pen.length; i++)
+for(var k = 0; k < pen.length; k++)
 {
-    pen[i].setAttribute("data-height", "400");
-    pen[i].setAttribute("data-default-tab", "result");
+    pen[k].setAttribute("data-height", "400");
+    pen[k].setAttribute("data-default-tab", "result");
 }
 
+function select()
+{
+    this.classList.add("selected");
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+    for(var j = 0; j < projects.length; j++)
+    {
+        if(projects[j] != this)
+        {
+            projects[j].classList.remove("selected");
+            links[j].style.display = "none";
+        }
+        else
+            links[j].style.display = "block";
+    }
+}
