53256bde0db9c8ad95ebb280d57655c92fdb842e
arpitmisraw <32339738+arpitmisraw@users.noreply.github.com>
2018-02-01 00:56:25 +0530

    Added some changes

--- script.js
+++ script.js
@@ -2,6 +2,24 @@ var projects = document.querySelectorAll(".projects li");
 var pen = document.querySelectorAll(".project-links p");
 var links = document.querySelectorAll(".links");
 
+function select()
+{
+    this.classList.add("selected");
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+    for(var j = 0; j < projects.length; j++)
+    {
+        if(projects[j] != this)
+        {
+            projects[j].classList.remove("selected");
+            links[j].style.display = "none";
+        }
+        else
+        {
+            links[j].style.display = "block";
+        }
+    }
+}
+
 
 
 for(var i = 0; i < projects.length; i++)
@@ -15,18 +33,4 @@ for(var k = 0; k < pen.length; k++)
     pen[k].setAttribute("data-default-tab", "result");
 }
 
-function select()
-{
-    this.classList.add("selected");
-    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-    for(var j = 0; j < projects.length; j++)
-    {
-        if(projects[j] != this)
-        {
-            projects[j].classList.remove("selected");
-            links[j].style.display = "none";
-        }
-        else
-            links[j].style.display = "block";
-    }
-}
+
